64bc61bfffea5aa169387534f824130be2916d3f
Ariel Fenster <arielfenster1610@gmail.com>
2025-02-14 11:41:32 +0200

    added reset db script

---
+++ server/database/scripts/clear.ts
@@ -0,0 +1,16 @@
+import { sql } from 'drizzle-orm';
+import { client, db } from '../db';
+
+async function main() {
+	if (!db._.schema) {
+		console.log('No db schema. aborting');
+		return;
+	}
+
+	await db.execute(sql.raw('DROP SCHEMA IF EXISTS "public" CASCADE;'));
+	await db.execute(sql.raw('CREATE SCHEMA public;'));
+
+	await client.end();
+}
+
+main();
