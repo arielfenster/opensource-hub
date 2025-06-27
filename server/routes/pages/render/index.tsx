fa7c78c234ba0e7fa9a1edbb8409eaa908b17321
Ariel Fenster <arielfenster1610@gmail.com>
2025-06-27 20:50:26 +0300

    nested server/render inside routes folder

---
+++ server/routes/pages/render/index.tsx
@@ -0,0 +1,51 @@
+import type { ReactNode } from 'react';
+import { renderToString } from 'react-dom/server';
+import { getScriptsFromManifest, type PageScripts } from '../../../../build-utils/manifest';
+import { buildEntryInputName, type AppPage } from '../../../../build-utils/paths';
+import { IS_PROD } from '../../../../shared/env';
+import { Html, type ServerPageProps } from './templates/html';
+import type { Context } from 'hono';
+import { usersHandler } from '../../../modules/users/users.handler';
+
+export type RenderServerPageProps = Pick<ServerPageProps, 'title' | 'clientData'> & {
+	page: AppPage;
+};
+
+export function renderServerPage(
+	component: ReactNode,
+	{ title, page, clientData }: RenderServerPageProps,
+) {
+	const pageScripts = getPageScripts(page);
+
+	return renderToString(
+		<Html title={title} pageScripts={pageScripts} clientData={clientData}>
+			{component}
+		</Html>,
+	);
+}
+
+function getPageScripts(page: AppPage): PageScripts {
+	if (IS_PROD) {
+		return getScriptsFromManifest(page);
+	} else {
+		return {
+			js: [buildEntryInputName(page)],
+			css: ['/client/index.css'],
+		};
+	}
+}
+
+export async function renderServerPageWithUser(
+	c: Context,
+	...args: Parameters<typeof renderServerPage>
+) {
+	const [component, serverPageProps] = args;
+	const user = await usersHandler.getCurrentUser(c);
+
+	if (user) {
+		serverPageProps.clientData = serverPageProps.clientData || {};
+		serverPageProps.clientData.user = user;
+	}
+
+	return renderServerPage(component, serverPageProps);
+}
