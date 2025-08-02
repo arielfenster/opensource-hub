59d645b5e8c2c9e06c9a9571a819c1cddf38de7e
Ariel Fenster <arielfenster1610@gmail.com>
2025-08-02 09:18:39 +0300

    removed container components

--- client/pages/auth/login/index.tsx
+++ client/pages/auth/login/index.tsx
@@ -1,10 +1,24 @@
 import { RpcQueryProvider } from '$/client/providers/rpc-query-provider';
-import { LoginContainer } from './container';
+import { LoginForm } from './form';
+import { useLogin } from './hook';
 
 export function LoginPage() {
+	const { login, loading, error } = useLogin();
+
 	return (
 		<RpcQueryProvider>
-			<LoginContainer />
+			<div className='mt-14 flex justify-center'>
+				<div className='w-full max-w-lg rounded bg-white p-8 shadow-xl'>
+					<h1 className='mb-6 text-center text-3xl font-bold text-blue-600'>Login</h1>
+					<LoginForm onSubmit={login} loading={loading} error={error} />
+					<p className='mt-4 text-center'>
+						Don't have an account?{' '}
+						<a href='/signup' className='text-blue-500 hover:text-blue-700'>
+							Sign up
+						</a>
+					</p>
+				</div>
+			</div>
 		</RpcQueryProvider>
 	);
 }
