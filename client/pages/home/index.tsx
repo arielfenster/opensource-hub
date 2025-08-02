6a825e09f44fc66a980db247374ce5c13a9738f9
Ariel Fenster <arielfenster1610@gmail.com>
2025-08-02 23:32:12 +0300

    refactored home page to use server-rendered query providers

--- client/pages/home/index.tsx
+++ client/pages/home/index.tsx
@@ -1,37 +1,35 @@
 import { Button } from '$/client/components/ui/button';
 import { getImagePath } from '$/client/lib/images';
-import { RpcQueryProvider } from '$/client/providers/rpc-query-provider';
 import { PageSection } from './page-section';
 
 export function HomePage() {
 	return (
-		<RpcQueryProvider>
-			<>
-				<div className='py-12'>
-					<h1 className='text-royal-blue mx-auto w-2/3 text-center text-5xl font-bold'>
-						{'Your one-stop shop for sharing and collaborating open source projects'}
-					</h1>
+		<>
+			<div className='py-12'>
+				<h1 className='text-royal-blue mx-auto w-2/3 text-center text-5xl font-bold'>
+					{'Your one-stop shop for sharing and collaborating open source projects'}
+				</h1>
+			</div>
+			<div className='mt-16 flex flex-col space-y-16'>
+				<div className='container mx-auto flex flex-col px-16'>
+					<PageSection
+						heading='Have an awesome idea for a project?'
+						subheading='Are you looking for more collaborators to work with you? Create your project and search for your dream team!'
+						image={getImagePath('bookshelf.svg')}
+					/>
+					<PageSection
+						heading='Expand your knowledge!'
+						subheading='Want to expand your knowledge and tech stack? Looking to dive deep into a framework but never had the opportunity? Search for projects by specific technologies to find exactly what you want!'
+						image={getImagePath('bookshelf.svg')}
+						orientation='flip'
+					/>
+					<PageSection
+						heading='Various 3rd-party tools integrations!'
+						subheading='Create your project with your favorite source control, chat and project management tools to bootstrap and kickstart your project!'
+						image={getImagePath('bookshelf.svg')}
+					/>
 				</div>
-				<div className='mt-16 flex flex-col space-y-16'>
-					<div className='container mx-auto flex flex-col px-16'>
-						<PageSection
-							heading='Have an awesome idea for a project?'
-							subheading='Are you looking for more collaborators to work with you? Create your project and search for your dream team!'
-							image={getImagePath('bookshelf.svg')}
-						/>
-						<PageSection
-							heading='Expand your knowledge!'
-							subheading='Want to expand your knowledge and tech stack? Looking to dive deep into a framework but never had the opportunity? Search for projects by specific technologies to find exactly what you want!'
-							image={getImagePath('bookshelf.svg')}
-							orientation='flip'
-						/>
-						<PageSection
-							heading='Various 3rd-party tools integrations!'
-							subheading='Create your project with your favorite source control, chat and project management tools to bootstrap and kickstart your project!'
-							image={getImagePath('bookshelf.svg')}
-						/>
-					</div>
-					{/* {Number(recentProjects?.length) > 0 && (
+				{/* {Number(recentProjects?.length) > 0 && (
 					<div className='bg-celestial-blue flex w-full flex-col items-center gap-4 p-8'>
 						<h3 className='text-ghost-white text-3xl font-semibold'>
 							Recently Added Projects
@@ -45,14 +43,13 @@ export function HomePage() {
 						</div>
 					</div>
 				)} */}
-					<div className='text-royal-blue flex flex-col items-center gap-4 text-center'>
-						<span className='text-3xl'>What are you waiting for? Join the Hub</span>
-						<a href='/login'>
-							<Button>Get started</Button>
-						</a>
-					</div>
+				<div className='text-royal-blue flex flex-col items-center gap-4 text-center'>
+					<span className='text-3xl'>What are you waiting for? Join the Hub</span>
+					<a href='/login'>
+						<Button>Get started</Button>
+					</a>
 				</div>
-			</>
-		</RpcQueryProvider>
+			</div>
+		</>
 	);
 }
--- client/roots/home.tsx
+++ client/roots/home.tsx
@@ -1,16 +1,23 @@
 import { hydrateRoot } from 'react-dom/client';
 import { HomePage } from '../pages/home';
-import { getPropertyFromClientData } from '../lib/window';
+import { getPropertyFromClientData, getWindow } from '../lib/window';
 import type { AuthenticatedUser } from '$/shared/types/users';
 import { Layout } from '../components/layout';
+import { RpcQueryProvider } from '../providers/rpc-query-provider';
+import { ReactQueryProvider } from '../providers/react-query-provider';
 
 import '../index.css';
 
 const user = getPropertyFromClientData<AuthenticatedUser>('user');
+const dehydratedState = getWindow().__PREFETCHED_STATE__;
 
 hydrateRoot(
 	document.getElementById('app')!,
-	<Layout user={user}>
-		<HomePage />
-	</Layout>,
+	<RpcQueryProvider>
+		<ReactQueryProvider dehydratedState={dehydratedState}>
+			<Layout user={user}>
+				<HomePage />
+			</Layout>
+		</ReactQueryProvider>
+	</RpcQueryProvider>,
 );
