3011e894686bda89fc05d65ee852f59640bb42aa
Ariel Fenster <arielfenster1610@gmail.com>
2025-10-18 23:33:43 +0300

    fixed rendering issue for technologies hook

--- client/hooks/useTechnologies.ts
+++ client/hooks/useTechnologies.ts
@@ -1,17 +1,17 @@
 import type { TechnologyData } from '$/shared/types/technologies';
-import { useCallback, useState } from 'react';
+import { useState } from 'react';
 import type { TechnologiesAutocompleteProps } from '../components/technologies-autocomplete';
 
 // TODO: should the selectedTechnologies state be in a global store?
 
-// TODO: maybe rename this to useTechnologiesState? useTechnologiesAutocomplete? useTechnologiesState?
+// TODO: maybe rename this to useTechnologiesState?
 
 type Props = Pick<TechnologiesAutocompleteProps, 'onSelect' | 'onRemove'>;
 
 export function useTechnologies({ onSelect, onRemove }: Props = {}) {
 	const [selectedTechnologies, setSelectedTechnologies] = useState<TechnologyData[]>([]);
 
-	const addTechnology = useCallback((technology: TechnologyData) => {
+	function addTechnology(technology: TechnologyData) {
 		setSelectedTechnologies((prev) => {
 			if (prev.find((tech) => tech.id === technology.id)) {
 				return prev;
@@ -22,14 +22,14 @@ export function useTechnologies({ onSelect, onRemove }: Props = {}) {
 			}
 			return [...prev, technology];
 		});
-	}, []);
+	}
 
-	const removeTechnology = useCallback((technology: TechnologyData) => {
+	function removeTechnology(technology: TechnologyData) {
 		setSelectedTechnologies((prev) => prev.filter((tech) => tech.id !== technology.id));
 		if (onRemove) {
 			onRemove(technology);
 		}
-	}, []);
+	}
 
 	return {
 		selectedTechnologies,
