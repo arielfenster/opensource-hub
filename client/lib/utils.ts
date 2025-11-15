bbf39b6b5b3039964ae741cb94d65193bf0f0afa
Ariel Fenster <arielfenster1610@gmail.com>
2025-11-15 18:15:50 +0200

    removed unused functions

--- client/lib/utils.ts
+++ client/lib/utils.ts
@@ -5,18 +5,6 @@ export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
 }
 
-// TODO: remove
-export function objectValues<TObj extends Record<string, string>>(
-	obj: TObj,
-): [TObj[keyof TObj], ...TObj[keyof TObj][]] {
-	return Object.values(obj) as any;
-}
-
-// TODO: remove
-export function objectValues2<TObj extends Record<string, string>>(obj: TObj): (keyof TObj)[] {
-	return Object.values(obj) as any;
-}
-
 export function parseDate(date: Date) {
 	return date.toLocaleDateString().split('/').reverse().join('/');
 }
