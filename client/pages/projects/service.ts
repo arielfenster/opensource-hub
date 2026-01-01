7f688d447ba9a97a3eee271e879b21acf73e048e
Ariel Fenster <arielfenster1610@gmail.com>
2026-01-02 00:13:22 +0200

    removed unused code from projects page

--- client/pages/projects/service.ts
+++ client/pages/projects/service.ts
@@ -1,19 +1,6 @@
 import type { ProjectDetails, ProjectTeamPosition } from '$/shared/types/projects';
 import type { TechnologyData } from '$/shared/types/technologies';
 
-// TODO: remove
-export type SearchFilterType = 'tech' | 'position';
-// TODO: remove
-export type SearchFilter =
-	| {
-			type: 'tech';
-			value: TechnologyData;
-	  }
-	| {
-			type: 'position';
-			value: ProjectTeamPosition;
-	  };
-
 export function getFilteredProjects(
 	projects: ProjectDetails[],
 	filterTechnologies: TechnologyData[],
